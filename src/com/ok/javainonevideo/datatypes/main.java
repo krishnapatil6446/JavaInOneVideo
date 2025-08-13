a237d6bbd306c50f86cc0a3d0f9fd0ed72a8db79
Krishna Patil <krishnapatil6446@gmail.com>
2025-08-13 21:09:08 +0530

    pdf file added for revision

--- src/com/ok/javainonevideo/datatypes/main.java
+++ src/com/ok/javainonevideo/datatypes/main.java
@@ -1,4 +1,39 @@
 package com.ok.javainonevideo.datatypes;
 
 public class main {
+    public static void main(String[]args){
+        System.out.println("Byte Range");
+        System.out.println("---------------");
+        byte a = 12;
+        System.out.println("Byte value: " +a);
+        System.out.println("Minimum Byte value: " +Byte.MIN_VALUE);
+        System.out.println("Maximum Byte value: " +Byte.MAX_VALUE);
+        System.out.println("---------------------------------------------------");
+
+
+        System.out.println("Short Range");
+        System.out.println("---------------");
+        short b = 13;
+        System.out.println("Short value: " +b);
+        System.out.println("Minimum Short value: " +Short.MIN_VALUE);
+        System.out.println("Maximum Short value: " +Short.MAX_VALUE);
+        System.out.println("---------------------------------------------------");
+
+
+        System.out.println("Integer Range");
+        System.out.println("---------------");
+        int c = 14;
+        System.out.println("Integer value: " +a);
+        System.out.println("Minimum Integer value: " +Integer.MIN_VALUE);
+        System.out.println("Maximum Integer value: " +Integer.MAX_VALUE);
+        System.out.println("---------------------------------------------------");
+
+        System.out.println("Long Range");
+        System.out.println("---------------");
+        long d = 9223372036854775807l;
+        System.out.println("Long value: " +a);
+        System.out.println("Minimum Long value: " +Long.MIN_VALUE);
+        System.out.println("Maximum Long value: " +Long.MAX_VALUE);
+        System.out.println("---------------------------------------------------");
+    }
 }
